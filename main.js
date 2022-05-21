75b12683b119f3e496b4a7d024f828369fa68e01
Sergio <sergioelisondo@gmail.com>
2022-05-21 16:30:29 -0700

    rhtyhm changed

--- main.js
+++ main.js
@@ -108,7 +108,7 @@ let melodyMix = tracery.createGrammar({
 
 // RHYTHM MAKER!
 let rhythmGrammar1 = tracery.createGrammar({
-  'groove': ['---_x_x', '-----xx_', 'x--x__-x', 'xxx__-x-'],
+  'groove': ['xxx_x_x', '-----xx_', 'x--x__-x', '--x_--x-'],
   'text_1': ['#groove#'],
 });
 
