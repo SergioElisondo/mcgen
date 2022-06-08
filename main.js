2e78c0d12873cad9247430a953882229a128ddc1
Sergio <sergioelisondo@gmail.com>
2022-06-07 22:55:47 -0700

    altered rhythm in melo gen

--- main.js
+++ main.js
@@ -108,7 +108,7 @@ let melodyMix = tracery.createGrammar({
 
 // RHYTHM MAKER!
 let rhythmGrammar1 = tracery.createGrammar({
-  'groove': ['xxx__--x', 'x--xxxx_', 'x---__--', '--x_-x--'],
+  'groove': ['x____--x', 'x-----x_', 'x---__--', 'xxx_-x--'],
   'text_1': ['#groove#'],
 });
 
