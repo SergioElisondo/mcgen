f7aa3f84255c472d0d9547de4195e1c9b48f74a0
Sergio <sergioelisondo@gmail.com>
2022-08-03 23:24:58 -0700

    changed the rhythm in melo gen

--- main.js
+++ main.js
@@ -108,7 +108,7 @@ let melodyMix = tracery.createGrammar({
 
 // RHYTHM MAKER!
 let rhythmGrammar1 = tracery.createGrammar({
-  'groove': ['---xx--x', 'x--x_x_x', '-x_-x-xx', 'x__-xxx-'],
+  'groove': ['xx-xx-xx', 'xx-xxx_x', 'xx_-x-x-', 'x__-x-x-'],
   'text_1': ['#groove#'],
 });
 
