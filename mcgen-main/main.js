29e3bf33e1f1bad89f63ec8b4161b60bd7c18703
Sergio Elisondo <71865570+SergioElisondo@users.noreply.github.com>
2022-10-06 11:26:16 -0700

    Update rhythm in melo gen

--- mcgen-main/main.js
+++ mcgen-main/main.js
@@ -108,7 +108,7 @@ let melodyMix = tracery.createGrammar({
 
 // RHYTHM MAKER!
 let rhythmGrammar1 = tracery.createGrammar({
-  'groove': ['x-x-x_x_x', 'x_xxx--x', 'x-x_-xxx', 'x_x-x---'],
+  'groove': ['xxx-x_x_x', 'x_x__--x', 'x-x_-x-x', 'x_x-x-xx'],
   'text_1': ['#groove#'],
 });
 
